17acfd3e056e51d0fdfb5b62dc05c9032d9d71c6
arthur <arthur.broudoux@gmail.com>
2024-10-21 17:55:19 +0200

    feat: decompose wizard component Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

---
+++ src/lib/components/wizard/WizardInput.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Label from "@/components/ui/label/label.svelte";
+  import Input from "@/components/ui/input/input.svelte";
+  import Textarea from "@/components/ui/textarea/textarea.svelte";
+  import Checkbox from "@/components/ui/checkbox/checkbox.svelte";
+
+  import type { WizardStepInput } from "@/utils/interfaces";
+
+  export let input: WizardStepInput;
+</script>
+
+{#if input.type === "text" || input.type === "textarea"}
+  <div class="w-full flex flex-col items-center justify-center gap-2 py-2">
+    <Label
+      for={input.id || input.name}
+      class="text-left w-full inline-flex gap-1"
+    >
+      {#if input.required}
+        <span class="text-red-600 text-md">*</span>{/if}
+      {input.label}
+    </Label>
+    {#if input.type === "text"}
+      <Input
+        type="text"
+        id={input.id || input.name}
+        name={input.name}
+        placeholder={input.placeholder}
+      />
+    {:else}
+      <Textarea
+        id={input.id || input.name}
+        name={input.name}
+        placeholder={input.placeholder}
+      />
+    {/if}
+    {#if input.description}
+      <p class="text-sm text-muted-foreground">
+        {input.description}
+      </p>
+    {/if}
+  </div>
+{:else if input.type === "checkbox"}
+  {#if input.description}
+    <p class="text-sm">{input.description}</p>
+  {/if}
+  <div class="w-full flex flex-row items-center justify-start gap-2">
+    <Checkbox id={input.id || input.name} name={input.name} />
+    <Label for={input.id || input.name} class="text-left inline-flex gap-1">
+      {input.label}
+    </Label>
+  </div>
+{/if}
--- src/lib/components/wizard/WizardTotal.svelte
+++ src/lib/components/wizard/WizardTotal.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { writable, type Writable } from "svelte/store";
 
-  import WizardStep from "$lib/components/wizard/WizardStep.svelte";
+  import Label from "@/components/ui/label/label.svelte";
+  import Input from "@/components/ui/input/input.svelte";
+  import Textarea from "@/components/ui/textarea/textarea.svelte";
+  import Checkbox from "@/components/ui/checkbox/checkbox.svelte";
   import * as Dialog from "$lib/components/ui/dialog/index";
   import { Button, buttonVariants } from "$lib/components/ui/button/index";
 
@@ -86,7 +89,68 @@
 
     {#each wizard.steps as step, index}
       {#if index === $activeStep}
-        <WizardStep {step} />
+        <div class="flex flex-col items-start justify-start gap-4 py-4">
+          <div class="inline-flex gap-2 font-medium">
+            {#if step.icon}
+              <svelte:component this={step.icon} />
+            {/if}
+            <p>{step.title}</p>
+          </div>
+          {#if step.description}
+            <p class="text-sm text-muted-foreground">{step.description}</p>
+          {/if}
+
+          {#each step.inputs as input}
+            {#if input.type === "text" || input.type === "textarea"}
+              <div
+                class="w-full flex flex-col items-center justify-center gap-2 py-2"
+              >
+                <Label
+                  for={input.id || input.name}
+                  class="text-left w-full inline-flex gap-1"
+                >
+                  {#if input.required}
+                    <span class="text-red-600 text-md">*</span>{/if}
+                  {input.label}
+                </Label>
+                {#if input.type === "text"}
+                  <Input
+                    type="text"
+                    id={input.id || input.name}
+                    name={input.name}
+                    placeholder={input.placeholder}
+                  />
+                {:else}
+                  <Textarea
+                    id={input.id || input.name}
+                    name={input.name}
+                    placeholder={input.placeholder}
+                  />
+                {/if}
+                {#if input.description}
+                  <p class="text-sm text-muted-foreground">
+                    {input.description}
+                  </p>
+                {/if}
+              </div>
+            {:else if input.type === "checkbox"}
+              {#if input.description}
+                <p class="text-sm">{input.description}</p>
+              {/if}
+              <div
+                class="w-full flex flex-row items-center justify-start gap-2"
+              >
+                <Checkbox id={input.id || input.name} name={input.name} />
+                <Label
+                  for={input.id || input.name}
+                  class="text-left inline-flex gap-1"
+                >
+                  {input.label}
+                </Label>
+              </div>
+            {/if}
+          {/each}
+        </div>
       {/if}
     {/each}
 
