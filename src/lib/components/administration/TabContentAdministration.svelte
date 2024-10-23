16661aa097f7bc777fa1f57733fa6f604bacfc40
arthur <arthur.broudoux@gmail.com>
2024-10-23 16:59:14 +0200

    feat: TabsComponent -> administration page Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

---
+++ src/lib/components/administration/TabContentAdministration.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Content from "$lib/components/ui/tabs/tabs-content.svelte";
+
+  import type { TabContentAdministration } from "$lib/utils/interfaces";
+
+  export let tab: TabContentAdministration;
+</script>
+
+<Content value={tab.value} class="py-2 space-y-1">
+  <h3 class="text-xl">{tab.title}</h3>
+  <p class="text-muted-foreground text-sm">{tab.description}</p>
+  <slot name="tabContent" />
+</Content>
---
+++ src/lib/components/administration/TabsAdministration.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import TabContentAdministration from "$lib/components/administration/TabContentAdministration.svelte";
+  import { Root, List, Trigger, Content } from "$lib/components/ui/tabs/index";
+</script>
+
+<Root value="users" class="py-4 w-full">
+  <List class="grid grid-cols-3 w-[400px]">
+    <Trigger value="users">Users</Trigger>
+    <Trigger value="snapshots">Snapshots</Trigger>
+    <Trigger value="secrets">Secrets</Trigger>
+  </List>
+  <TabContentAdministration
+    tab={{
+      value: "users",
+      title: "Users Management",
+      description:
+        "Manage the users of the Keycloak / RH-SSO realm, retrieving and assigning them roles.",
+    }}
+  />
+  <TabContentAdministration
+    tab={{
+      value: "snapshots",
+      title: "Import Snapshots",
+      description:
+        "Upload and import a previously exported JSON repository snapshot.",
+    }}
+  />
+  <TabContentAdministration
+    tab={{
+      value: "secrets",
+      title: "Authentication Secrets",
+      description:
+        "Manage the Authentication options available for different importers.",
+    }}
+  />
+</Root>
