fb26ea6b9655af44f057ff7ad7035a2af3e8d722
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-01-07 15:17:28 +0100

    feat: #8 Adding test details page structure Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- src/routes/tests/[testId]/+page.ts
+++ src/routes/tests/[testId]/+page.ts
@@ -2,6 +2,8 @@
 // it so that it gets served as a static asset in production
 export const prerender = false;
 
+import { getService } from "$lib/services/services.service";
+import { getTestResult } from "$lib/services/tests.service";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ parent, fetch, params }) => {
@@ -9,8 +11,11 @@ export const load: PageLoad = async ({ parent, fetch, params }) => {
   let parentData = await parent();
 
   console.log("In /tests/[id] PageLoad(), after parent load");
-  
+  const test = await getTestResult(params.testId);
+
   const data = {
+    test: test,
+    service: getService(test.serviceId),
   };
 
   return data;
