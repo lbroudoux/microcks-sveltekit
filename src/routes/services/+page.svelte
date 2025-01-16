67cfbed35fba168a88dd03c8784e70f9e5fdf516
arthur <arthur.broudoux@gmail.com>
2025-01-16 23:57:39 +0100

    style: clear services table Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -55,35 +55,31 @@
   {#await data.services}
     <Loader />
   {:then services}
-    <Table.Root>
+    <Table.Root class="my-4">
       {#each services as service}
         <Table.Row>
           <Table.Cell>
-            <ApiTypeIcon type="{ service.type }" />
+            <ApiTypeIcon type="{service.type}" />
           </Table.Cell>
           <Table.Cell>
             <h4>
-              <a href="/services/{ service.id }" class="font-semibold text-base">
+              <a href="/services/{service.id}" class="font-semibold text-base">
                 { service.name }
               </a>
             </h4>
           </Table.Cell>
           <Table.Cell>
-            <ApiTypeBadge type="{ service.type }" />
-            <span class="text-base font-regular">
-              | v{ service.version } |
+            <ApiTypeBadge type="{service.type}" />
+            <span class="text-base font-regular ml-2">
+              v{service.version} | Updated on {service.metadata.lastUpdate}
             </span>
-            <!-- <br />
-            <div style="margin-top: 10px">
-              <small>Updated on { service.metadata.lastUpdate }</small>
-            </div> -->
           </Table.Cell>
           <Table.Cell class="flex flex-row gap-1 items-center justify-center">
-              <Cog />{ service.operations.length } Operations
+              <Cog />{service.operations.length} Operations
           </Table.Cell>
           <Table.Cell class="text-right">
             <Button variant="outline">
-              <a href={`/services/${service.id}`}> Details</a>
+              <a href={`/services/${service.id}`}>Details</a>
             </Button>
             <DropdownMenu.Root>
               <DropdownMenu.Trigger>
@@ -102,47 +98,49 @@
       {/each}
     </Table.Root>
 
-    <div>
-      {#await data.servicesCount}
-      <Loader />
-      {:then servicesCount}
-        Got { servicesCount.counter } services
-      {:catch error}
-        <p>error loading services: {error.message}</p>
-      {/await}  
-    </div>
+    <div class="w-full flex flex-row items-center justify-between py-4">
+      <p class="flex flex-row items-center justify-start">
+        {#await data.servicesCount}
+        <Loader />
+        {:then servicesCount}
+          Got { servicesCount.counter } services
+        {:catch error}
+          error loading services: {error.message}
+        {/await}
+      </p>
 
-    <Pagination.Root {count} perPage={perPage} siblingCount={siblingCount} onPageChange={(page) => pageChange(page)}>
-      {#snippet children({ pages, currentPage })}
-        <Pagination.Content>
-          <Pagination.Item>
-            <Pagination.PrevButton>
-              <ChevronLeft class="size-4" />
-              <span class="hidden sm:block">Previous</span>
-            </Pagination.PrevButton>
-          </Pagination.Item>
-          {#each pages as page (page.key)}
-            {#if page.type === "ellipsis"}
-              <Pagination.Item>
-                <Pagination.Ellipsis />
-              </Pagination.Item>
-            {:else}
-              <Pagination.Item>
-                <Pagination.Link {page} isActive={currentPage === page.value}>
-                  {page.value}
-                </Pagination.Link>
-              </Pagination.Item>
-            {/if}
-          {/each}
-          <Pagination.Item>
-            <Pagination.NextButton>
-              <span class="hidden sm:block">Next</span>
-              <ChevronRight class="size-4" />
-            </Pagination.NextButton>
-          </Pagination.Item>
-        </Pagination.Content>
-      {/snippet}
-    </Pagination.Root>
+      <Pagination.Root {count} perPage={perPage} siblingCount={siblingCount} onPageChange={(page) => pageChange(page)}>
+        {#snippet children({ pages, currentPage })}
+          <Pagination.Content>
+            <Pagination.Item>
+              <Pagination.PrevButton>
+                <ChevronLeft class="size-4" />
+                <span class="hidden sm:block">Previous</span>
+              </Pagination.PrevButton>
+            </Pagination.Item>
+            {#each pages as page (page.key)}
+              {#if page.type === "ellipsis"}
+                <Pagination.Item>
+                  <Pagination.Ellipsis />
+                </Pagination.Item>
+              {:else}
+                <Pagination.Item>
+                  <Pagination.Link {page} isActive={currentPage === page.value}>
+                    {page.value}
+                  </Pagination.Link>
+                </Pagination.Item>
+              {/if}
+            {/each}
+            <Pagination.Item>
+              <Pagination.NextButton>
+                <span class="hidden sm:block">Next</span>
+                <ChevronRight class="size-4" />
+              </Pagination.NextButton>
+            </Pagination.Item>
+          </Pagination.Content>
+        {/snippet}
+      </Pagination.Root>
+    </div>
 
     <!-- <TableServices {services} /> -->
 
@@ -189,114 +187,3 @@
     <p>error loading services: {error.message}</p>
   {/await}
 </PageLayout>
-
-<!-- <style>
-.serviceName {
-  flex-basis: 40%;
-  max-width: 300px;
-  display: inline-block;
-  word-wrap: break-word;
-  word-break: break-all;
-}
-.serviceMetadata {
-  flex-basis: 60%;
-  display: inline-block;
-  word-wrap: break-word;
-  word-break: break-all;
-}
-
-.list-view-pf-view {
-  background: #ededed;
-  border: none;
-  margin-top: 30px;
-}
-
-.list-group {
-  padding-left: 0;
-  margin-bottom: 20px;
-}
-.list-group-item {
-  position: relative;
-  display: block;
-  padding: 10px 15px;
-  margin-bottom: -1px;
-  background-color: #fff;
-  border: 1px solid #f5f5f5;
-}
-
-.list-view-pf-main-info {
-  align-items: flex-start;
-  display: flex;
-  flex: 1;
-  min-width: 0;
-  padding-bottom: 20px;
-  padding-top: 20px;
-}
-
-.list-view-pf-left {
-  display: table-cell;
-  padding-right: 20px;
-  text-align: center;
-  vertical-align: top;
-}
-
-.list-view-pf-body {
-  -ms-flex-align: center;
-  -ms-grid-row-align: center;
-  align-items: center;
-  display: table-cell;
-  flex: 1;
-  min-width: 0;
-  vertical-align: top;
-  width: 100%;
-}
-
-.list-view-pf-description {
-  flex: 1 0 50%;
-  flex-basis: 70%;
-}
-
-.list-view-pf-additional-info {
-  align-items: center;
-  display: flex;
-  flex-wrap: wrap;
-}
-
-.list-view-pf-additional-info-item {
-  align-items: center;
-  display: inline-block;
-  display: flex;
-  margin-right: 20px;
-  max-width: 100%;
-  text-align: center;
-}
-
-.list-view-pf-actions {
-  float: right;
-  margin-bottom: 20px;
-  margin-left: 20px;
-  margin-top: 20px;
-  order: 2;
-}
-
-@media (min-width: 992px) {
-  .list-view-pf-body {
-    align-items: center;
-    display: flex;
-    flex-direction: row;
-  }
-
-  .list-view-pf-description {
-    align-items: center;
-    display: flex;
-    float: left;
-    width: 50%;
-  }
-
-  .list-view-pf-additional-info {
-    flex: 1 0 auto;
-    float: left;
-    width: 50%;
-  }
-}
-</style> -->
