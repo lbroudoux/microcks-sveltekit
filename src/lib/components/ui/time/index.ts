a133cbb5675203e010ce583438b8156f46de490b
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-01-17 16:18:56 +0100

    feat: #7 Implement timeAgo and mediumDate libs Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

---
+++ src/lib/components/ui/time/index.ts
@@ -0,0 +1,10 @@
+import MediumDate from "./medium-date.svelte"
+import TimeAgo from "./time-ago.svelte";
+
+export {
+  TimeAgo,
+  MediumDate,
+
+  //
+  MediumDate as default,
+};
---
+++ src/lib/components/ui/time/medium-date.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  interface Props {
+    ts: number;
+  }
+
+  let { ts }: Props = $props();
+
+  const format = new Intl.DateTimeFormat('en-us', {
+    dateStyle: 'medium',
+    timeStyle: 'medium'
+  });
+
+  function formatDateTime() {
+    const date = new Date(ts);
+    return format.format(date);
+  }
+</script>
+
+{formatDateTime()}
+
