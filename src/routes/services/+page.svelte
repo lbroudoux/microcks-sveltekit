a29632736b7893d3169f85437664e68e50c04ca1
Laurent Broudoux <laurent.broudoux@gmail.com>
2025-01-20 11:24:15 +0100

    feat: #12 WIP on Direct API Wizard Signed-off-by: Laurent Broudoux <laurent.broudoux@gmail.com>

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -10,6 +10,7 @@
   import Loader from "$lib/components/global/Loader.svelte";
   import Button from "$lib/components/ui/button/button.svelte";
   import * as Table from "$lib/components/ui/table";
+  import * as Tooltip from "$lib/components/ui/tooltip";
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
   import * as Pagination from "$lib/components/ui/pagination";
   import { shServices } from "$lib/utils/constants";
@@ -19,14 +20,17 @@
   import ApiTypeIcon from "$lib/components/ui/api-type-icon/api-type-icon.svelte";
 
   import { getServices } from "$lib/services/services.service";
+  import MediumDate from "$lib/components/ui/time";
+  import { Wizard, Wizard2, WizardStep, type WizardStepConfig } from "$lib/components/ui/wizard2";
+    import DirectApiWizard from "./direct-api-wizard.svelte";
+    
 
   let { data }: { data: PageData } = $props();
 
-  const services: Service[] = data.services;
-  const servicesCount: Promise<number> = data.servicesCount;
+  let services: Service[] = $state(data.services);
   const servicesLabels: Promise<Map<string, string[]>> = data.servicesLabels;
+
   const isDesktop = new MediaQuery("(min-width: 768px)");
-  const count = 18;
   const perPage = $derived(isDesktop.current ? 5 : 3);
   const siblingCount = $derived(isDesktop.current ? 2 : 1);
 
@@ -34,82 +38,149 @@
     console.log("In page /services onMount()");
   });
 
-  function selectService(service: Service) {
-    console.log("Selected service: ", service);
+  function pageChange(page: number) {
+    getServices(page, 5).then((newServices) => {
+      services = newServices;
+    });
   }
 
-  function pageChange(page: number) {
-    console.log("Page changed to: ", page);
-    getServices(page, 5).then((services) => {
-      data.services = services;
-    });
+  let wizardIsOpen: boolean = $state(false);
+  let wizard2IsOpen: boolean = $state(false);
+
+  const step1Config: WizardStepConfig = {
+    id: 'step1',
+    title: 'Direct API type',
+    nextEnabled: true,
+    expandReviewDetails: true,
+  };
+  const step2Config: WizardStepConfig = {
+    id: 'step2',
+    title: 'API properties',
+    nextEnabled: true,
+    expandReviewDetails: true,
+  };
+
+  function openCreateDirectAPI(): void {
+    //wizardIsOpen = true;
+    wizard2IsOpen = true;
   }
 </script>
 
+{#snippet step1View()}
+  <h3>Step 1</h3>
+{/snippet}
+{#snippet step2View()}
+  <h3>Step 2</h3>
+{/snippet}
+
+{#snippet step1Review()}
+  <li>Review of Step 1</li>
+{/snippet}
+{#snippet step2Review()}
+  <li>Review of Step 2</li>
+{/snippet}
+
+<DirectApiWizard isOpen={wizard2IsOpen} />
+
 <PageLayout>
   <SectionHeading sectionHeading={shServices}>
-    <!-- {#snippet headingActions()}
-      <Button >+ Add Direct API</Button>
-    {/snippet} -->
+    {#snippet headingActions()}
+      <Button onclick={openCreateDirectAPI}>+ Add Direct API</Button>
+    {/snippet}
   </SectionHeading>
-  {#await data.services}
-    <Loader />
-  {:then services}
-    <Table.Root class="my-4">
-      {#each services as service}
-        <Table.Row>
-          <Table.Cell>
-            <ApiTypeIcon type="{service.type}" />
-          </Table.Cell>
-          <Table.Cell>
-            <h4>
-              <a href="/services/{service.id}" class="font-semibold text-base">
-                { service.name }
-              </a>
-            </h4>
-          </Table.Cell>
-          <Table.Cell>
-            <ApiTypeBadge type="{service.type}" />
-            <span class="text-base font-regular ml-2">
-              v{service.version} | Updated on {service.metadata.lastUpdate}
-            </span>
-          </Table.Cell>
-          <Table.Cell class="flex flex-row gap-1 items-center justify-center">
-              <Cog />{service.operations.length} Operations
-          </Table.Cell>
-          <Table.Cell class="text-right">
-            <Button variant="outline">
-              <a href={`/services/${service.id}`}>Details</a>
-            </Button>
-            <DropdownMenu.Root>
-              <DropdownMenu.Trigger>
-                <span class="sr-only">Open menu</span>
-                <Ellipsis class="h-4 w-4" />
-              </DropdownMenu.Trigger>
-              <DropdownMenu.Content>
-                <DropdownMenu.Item>
-                  <Delete class="mr-2 size-4" />
-                  <span>Delete</span>
-                </DropdownMenu.Item>
-              </DropdownMenu.Content>
-            </DropdownMenu.Root>
-          </Table.Cell>
-        </Table.Row> 
-      {/each}
-    </Table.Root>
 
-    <div class="w-full flex flex-row items-center justify-between py-4">
-      <p class="flex flex-row items-center justify-start">
-        {#await data.servicesCount}
-        <Loader />
-        {:then servicesCount}
-          Got { servicesCount.counter } services
-        {:catch error}
-          error loading services: {error.message}
-        {/await}
-      </p>
+  <!-- 
+  <Wizard isOpen={wizardIsOpen}>
+    <WizardStep config={step1Config}>
+      {#snippet view()}
+        <h3>Step 1</h3>
+      {/snippet}
 
-      <Pagination.Root {count} perPage={perPage} siblingCount={siblingCount} onPageChange={(page) => pageChange(page)}>
+      {#snippet review()}
+        <li>Review of Step 1</li>
+      {/snippet}
+    </WizardStep>
+    <WizardStep config={step2Config}>
+      {#snippet view()}
+        <h3>Step 2</h3>
+      {/snippet}
+
+      {#snippet review()}
+        <li>Review of Step 2</li>
+      {/snippet}
+    </WizardStep>
+  </Wizard>
+  -->
+
+  <!-- 
+  <Wizard2 isOpen={wizardIsOpen} stepConfigs={[step1Config, step2Config]} 
+      stepViews={[step1View, step2View]} 
+      stepReviews={[step1Review, step2Review]}>
+  </Wizard2>
+  -->
+  
+  <Table.Root class="my-4">
+    {#each services as service}
+      <Table.Row>
+        <Table.Cell>
+          <ApiTypeIcon type="{service.type}" />
+        </Table.Cell>
+        <Table.Cell>
+          <h4>
+            <a href="/services/{service.id}" class="font-semibold text-base">
+              { service.name }
+            </a>
+          </h4>
+        </Table.Cell>
+        <Table.Cell>
+          <ApiTypeBadge type="{service.type}" />
+          | Version {service.version} | 
+          <br/> 
+          Updated on <MediumDate ts={service.metadata.lastUpdate}/>
+        </Table.Cell>
+        <Table.Cell class="flex flex-row gap-1 items-center justify-center">
+          <Tooltip.Provider>
+            <Tooltip.Root delayDuration={200}>
+              <Tooltip.Trigger>
+                <Cog /><strong>{service.operations.length}</strong> Operations
+              </Tooltip.Trigger>
+              <Tooltip.Content>
+                Operations
+                <ul>
+                  {#each service.operations as operation}
+                    <li>{operation.name}</li>
+                  {/each}
+                </ul>
+              </Tooltip.Content>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        </Table.Cell>
+        <Table.Cell class="text-right">
+          <Button variant="outline">
+            <a href={`/services/${service.id}`}>Details</a>
+          </Button>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <span class="sr-only">Open menu</span>
+              <Ellipsis class="h-4 w-4" />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Item>
+                <Delete class="mr-2 size-4" />
+                <span>Delete</span>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        </Table.Cell>
+      </Table.Row> 
+    {/each}
+  </Table.Root>
+
+  <div class="w-full flex flex-row items-center justify-between py-4">
+    {#await data.servicesCount}
+      <Loader />
+    {:then servicesCount}
+      <Pagination.Root count={servicesCount.counter} perPage={perPage} siblingCount={siblingCount} onPageChange={(page) => pageChange(page)}>
         {#snippet children({ pages, currentPage })}
           <Pagination.Content>
             <Pagination.Item>
@@ -140,50 +211,49 @@
           </Pagination.Content>
         {/snippet}
       </Pagination.Root>
-    </div>
+    {:catch error}
+      Error loading number of services: {error.message}
+    {/await}
+  </div>
 
-    <!-- <TableServices {services} /> -->
+  <!-- <TableServices {services} /> -->
 
-    <!--
-    <div class="list-group list-view-pf list-view-pf-view">
-      {#each services as service}
-        <div class="list-group-item">
-          <div class="list-view-pf-main-info" onmouseenter={() => selectService(service)}>
-            <div class="list-view-pf-left">
-              <ApiTypeIcon type="{ service.type }" />
-            </div>
+  <!--
+  <div class="list-group list-view-pf list-view-pf-view">
+    {#each services as service}
+      <div class="list-group-item">
+        <div class="list-view-pf-main-info" onmouseenter={() => selectService(service)}>
+          <div class="list-view-pf-left">
+            <ApiTypeIcon type="{ service.type }" />
+          </div>
 
-            <div class="list-view-pf-body">
-              <div class="list-view-pf-description">
-                <div class="list-group-item-heading serviceName">
-                  <h4><a href="/services/{ service.id }">{ service.name }</a></h4>
-                </div>
-                <div class="list-group-item-text serviceMetadata">
-                  <ApiTypeBadge type="{ service.type }" />
-                  | Version <b>{ service.version }</b>
-                  | 
-                  <br />
-                  <div style="margin-top: 10px">
-                    <small>Updated on { service.metadata.lastUpdate }</small>
-                  </div>
-                </div>
+          <div class="list-view-pf-body">
+            <div class="list-view-pf-description">
+              <div class="list-group-item-heading serviceName">
+                <h4><a href="/services/{ service.id }">{ service.name }</a></h4>
               </div>
-              <div class="list-view-pf-additional-info">
-                <div class="list-view-pf-additional-info-item" placement="right">
-                  <Cog /> <strong>{ service.operations.length }</strong>  Operations
+              <div class="list-group-item-text serviceMetadata">
+                <ApiTypeBadge type="{ service.type }" />
+                | Version <b>{ service.version }</b>
+                | 
+                <br />
+                <div style="margin-top: 10px">
+                  <small>Updated on { service.metadata.lastUpdate }</small>
                 </div>
               </div>
             </div>
-
-            <div class="list-view-pf-actions">
+            <div class="list-view-pf-additional-info">
+              <div class="list-view-pf-additional-info-item" placement="right">
+                <Cog /> <strong>{ service.operations.length }</strong>  Operations
+              </div>
             </div>
           </div>
-        </div>
-      {/each}
-    </div>
-    -->
 
-  {:catch error}
-    <p>error loading services: {error.message}</p>
-  {/await}
+          <div class="list-view-pf-actions">
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+  -->
 </PageLayout>
--- src/routes/services/[id]/+page.svelte
+++ src/routes/services/[id]/+page.svelte
@@ -3,37 +3,40 @@
   import type { PageData } from './$types';
   import { onMount } from 'svelte';
 
+  import PageLayout from "$lib/components/layouts/PageLayout.svelte";
+  import SectionHeading from "$lib/components/layouts/SectionHeading.svelte";
+  import { MediumDate, TimeAgo } from "$lib/components/ui/time";
+
+  import type { Service } from "$lib/models/service.model";
+
   interface Props {
     data: PageData;
   }
 
   let { data }: Props = $props();
 
+  let service: Service = $state(data.service);
+
   onMount(() => {
     console.log("In page /services/[id] onMount()");
   })
 </script>
 
-<div>
-  {#await data.service}
-    Loading service...
-  {:then service}
-    <h1>Service {service.name} - {service.version}</h1>
-    <small>Created {service.metadata.createdOn}, last update on {service.metadata.lastUpdate}</small>
+<PageLayout>
+  
+  <h1>Service {service.name} - {service.version}</h1>
+  <small>Created <TimeAgo ts={service.metadata.createdOn}/>, last update on <MediumDate ts={service.metadata.lastUpdate}/></small>
 
-    <h3>Properties</h3>
-    <dl>
-      <dt>Service Type:</dt>
-      <dd><ApiTag type="{service.type}"/></dd>
-    </dl>
+  <h3>Properties</h3>
+  <dl>
+    <dt>Service Type:</dt>
+    <dd><ApiTag type="{service.type}"/></dd>
+  </dl>
 
-    <div>
-      <a href="/tests/service/{service.id}">View Tests Runs</a>
-    </div>
-  {:catch error}
-    <p>error loading service: {error.message}</p>
-  {/await}
-</div>   
+  <div>
+    <a href="/tests/service/{service.id}">View Tests Runs</a>
+  </div>
+</PageLayout>
 
 <style>
 
